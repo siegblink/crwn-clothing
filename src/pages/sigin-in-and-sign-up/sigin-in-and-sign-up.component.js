6911cf85d15191a74516a76f518098efabe9afb7
siegblink <siegblink@gmail.com>
2019-08-12 11:23:06 +0800

    Update SignInAndSignUpPage component

--- src/pages/sigin-in-and-sign-up/sigin-in-and-sign-up.component.js
+++ src/pages/sigin-in-and-sign-up/sigin-in-and-sign-up.component.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './sigin-in-and-sign-up.styles.scss'
 import SignIn from '../../components/sigin-in/signin-component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 export default function SignInAndSignUpPage() {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   )
 }
