6f67f6826457ae6de16f9c6cb50434e98795b058
Siegfred Balona <siegblink@gmail.com>
2019-08-29 05:44:06 +0800

    Fix another filename errors

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './sign-in-and-sign-up.styles.scss'
-import SignIn from '../../components/sign-in/sign-in-component'
+import SignIn from '../../components/sign-in/sign-in.component'
 import SignUp from '../../components/sign-up/sign-up.component'
 
 export default function SignInAndSignUpPage() {
